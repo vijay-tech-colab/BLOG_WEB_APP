635b1470355fb2d8365fd119ef13796e41830dbc
vijay kumar <vijaykumar06232@gmail.com>
2025-08-04 22:50:52 +0530

    added folder structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import { LoginForm } from "@/components/login-form";
+
+export default function App() {
+  return (
+    <div className="relative bg-gradient-soft flex min-h-svh w-full items-center justify-center p-6 md:p-10 flex-col md:flex-row gap-10 h-[100vh]">
+      {/* Image above form on small screens, beside on large */}
+      <div className="relative w-full max-w-lg flex justify-center items-center md:order-1 -mb-5 sm:mb-0 z-10">
+        <img
+          src="/images/login-girl.png"
+          alt="Login Illustration"
+          className="w-[300px] sm:w-[400px] md:w-[500px] object-contain drop-shadow-xl"
+        />
+      </div>
+
+      {/* Login Form (visually behind image on small screens) */}
+      <div className="relative z-0 w-full max-w-sm mt-[-40px] sm:mt-0">
+        <LoginForm />
+      </div>
+    </div>
+  );
+}
---
+++ frontend/src/components/login-form.jsx
@@ -0,0 +1,91 @@
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useState } from "react";
+
+export function LoginForm({ className, ...props }) {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setFormData((pre) => ({...pre , [name] : value}));
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  }
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card className="bg-neumorphism ">
+        <CardHeader>
+          <CardTitle>Login to your account</CardTitle>
+          <CardDescription>
+            Enter your email below to login to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={handleSubmit}>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-3">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  name="email"
+                  value={formData.email}
+                  required
+                  onChange={handleChange}
+                  autoComplete="on"
+                />
+              </div>
+              <div className="grid gap-3">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                  <a
+                    href="#"
+                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                  >
+                    Forgot your password?
+                  </a>
+                </div>
+                <Input
+                  id="password"
+                  type="password"
+                  name="password"
+                  value={formData.password}
+                  required
+                  autoComplete="on"
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="flex flex-col gap-3">
+                <Button type="submit" className="w-full" variant="outline">
+                  Login
+                </Button>
+              </div>
+            </div>
+            <div className="mt-4 text-center text-sm">
+              Don&apos;t have an account?{" "}
+              <a href="#" className="underline underline-offset-4">
+                Sign up
+              </a>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
